752d8fee94c61daeb16b1bf1c992b9bb1244d894
Mounika70324 <136341776+Mounika70324@users.noreply.github.com>
2026-08-23 12:42:15 +0530

    Update main.tf

--- Jerney-devops/Jerney-devops/terraform/main.tf
+++ Jerney-devops/Jerney-devops/terraform/main.tf
@@ -38,6 +38,19 @@ module "vpc" {
   }
 }
 
+# ----create s3 bucket ----
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "vault-demo-bucket-${random_id.bucket_suffix.hex}"
+}
+
 # ---- EKS Cluster (Auto Mode) ----
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
